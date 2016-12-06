4ca9f87f5ec6e6af975c246259fbe355d783e77d
MjaJong <menno.dejong@ziggo.nl>
2016-12-06 13:35:28 +0100

    I didn't fuck up for once

--- Turf Wars/Turf Wars/GameLogic.cs
+++ Turf Wars/Turf Wars/GameLogic.cs
@@ -1,7 +1,19 @@
-﻿namespace Turf_Wars
+﻿using System.Collections.Generic;
+using Turf_Wars.Pages;
+
+namespace Turf_Wars
 {
     public class GameLogic
     {
+        public PUP CurrentPoint { get; set; }
         
+        private List<Player> players;
+        //private readonly BingMapsWrapper MapWrapper;
+        private readonly IPageHandler pageHandler;
+
+        public GameLogic()
+        {
+            
+        }
     }
 }
--- Turf Wars/Turf Wars/PUP.cs
+++ Turf Wars/Turf Wars/PUP.cs
@@ -1,7 +1,22 @@
-﻿namespace Turf_Wars
+﻿using Turf_Wars.Teams;
+
+namespace Turf_Wars
 {
     public class PUP
     {
-        
+        private int RedScore { get; set; }
+        private int BlueScore { get; set; }
+        private int YellowScore { get; set; }
+        //private readonly GeoLocation location { get;}
+
+        public PUP()
+        {
+            
+        }
+
+        private void AwardPoints(Team team, int score)
+        {
+            
+        }
     }
 }
