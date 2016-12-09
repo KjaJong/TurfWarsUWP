c79efb3a71dd5438cc615939952dc4c41b3c2018
MjaJong <menno.dejong@ziggo.nl>
2016-12-09 11:48:26 +0100

    Fixed fuckups

--- Turf Wars/Turf Wars/Pages/MapPage.xaml.cs
+++ Turf Wars/Turf Wars/Pages/MapPage.xaml.cs
@@ -22,9 +22,9 @@ namespace Turf_Wars.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class GamePage : Page
+    public sealed partial class MapPage : Page
     {
-        public GamePage()
+        public MapPage()
         {
             this.InitializeComponent();
         }
