20be26527752480baed8fcabcdf49ce4a8702c15
MjaJong <menno.dejong@ziggo.nl>
2016-12-09 13:51:46 +0100

    Added a few things here and there

--- Turf Wars/Turf Wars/GameLogic.cs
+++ Turf Wars/Turf Wars/GameLogic.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Windows.Devices.Geolocation.Geofencing;
 using Turf_Wars.Pages;
 using Turf_Wars.Teams;
 
@@ -8,12 +9,32 @@ namespace Turf_Wars
     {
         public PUP CurrentPoint { get; set; }
 
+        private List<CapturePoint> _points;
+
         public static List<Player> Players = new List<Player>();
-        //private readonly BingMapsWrapper MapWrapper;
+        private readonly BingMapsWrapper MapWrapper;
         private readonly IPageHandler _pageHandler;
 
         public GameLogic()
         {
+            _points = new List<CapturePoint>();
+        }
+
+        public void AddCapturePoint(Geofence f, int reward)
+        {
+            _points.Add(new CapturePoint(f, reward));
+        }
+    }
+
+    public class CapturePoint
+    {
+        public Geofence Point { get; }
+        public readonly int reward;
+
+        public CapturePoint(Geofence fence, int reward)
+        {
+            Point = fence;
+            this.reward = reward;
         }
     }
 }
---
+++ Turf Wars/Turf Wars/MapCommander.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Devices.Geolocation.Geofencing;
+using Windows.UI.Xaml.Controls.Maps;
+
+namespace Turf_Wars
+{
+    class MapCommander
+    {
+        private readonly MapControl _controller;
+        private List<Geopoint> _points;
+        private List<Geofence> _fences;
+
+        public MapCommander(MapControl c)
+        {
+            _controller = c;
+            _points = new List<Geopoint>();
+            _fences = new List<Geofence>();
+        }
+
+        /// <summary>
+        /// This adds a geopoint to the list
+        /// </summary>
+        /// <param name="latitude"> Latitude of the point. </param> //TODO enter a reference to the paramater between which it must fall
+        /// <param name="longitude"> Longitude of the point </param> //TODO see latitude
+        public void AddPoint(double latitude, double longitude)
+        {
+            _points.Add(new Geopoint(new BasicGeoposition()
+            {
+                Latitude = latitude,
+                Longitude = longitude
+            }));
+        }
+
+        ///<summary>
+        /// This creates a geofence for the list. The point are never single use, so manual removal is neccesary.
+        /// </summary>
+        /// <param name="fenceId">The name of the fence</param>
+        /// <param name="p">The location of the fence as a geopoint</param>
+        /// <param name="radius">The radius of the circle of the fence</param>
+        /// <param name="state">The monitor state. It can monitor for entry, exit, removal of the point or have no monitor</param>
+        /// <param name="t">The timespan that indicates how long a device needs to be inside the fence to trigger the event.</param>
+        public void CreateGeofence(string fenceId, Geopoint p, double radius, MonitoredGeofenceStates state, TimeSpan t)
+        {
+            BasicGeoposition bgp = new BasicGeoposition();
+            bgp.Latitude = p.Position.Latitude;
+            bgp.Longitude = p.Position.Longitude;
+            bgp.Altitude = p.Position.Altitude;
+
+            _fences.Add(new Geofence(fenceId, new Geocircle(bgp, radius), state, false, t));
+        }
+    }
+}
